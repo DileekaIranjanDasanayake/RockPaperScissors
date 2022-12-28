7a373ac8d3a5286a0dd4751649bf8a65ba36a532
Dileeka <dileekairanjandasanayake@gmail.com>
2022-12-28 16:12:41 +0530

    Added basic ui elements to the game

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -1,10 +1,41 @@
-let computerValue;
+document.addEventListener('DOMContentLoaded', ()=>{
 
-let playerValue;
 
-let result;
+  let playerValue= ''
+  let computerValue = '';
+  let result = '';
+  const buttons = ["Rock","Paper","Scissor"]
+  let random = ''
+  let word = ''
+  const nButtons = 3
+  let resultHeading = ''
+  let computerChoiceHeading = ''
+  for(let i = 0; i<nButtons; i++ ){
+    random = Math.floor(Math.random()*(buttons.length-1))+1
+    word = buttons[random]
+    const newButton = document.createElement('button')
+    newButton.innerText=buttons[i]
+    document.body.appendChild(newButton)
+  }
+  let Mybuttons = document.querySelectorAll('button')
+  Mybuttons.forEach(i=>{
+    i.addEventListener('click',(e)=>{
+      playerValue= e.target.innerText
+      computerValue = getComputerChoice().toUpperCase();
+      result = round(playerValue.toUpperCase(),computerValue);
+      console.log(playerValue)
+      console.log(computerValue)
+      console.log(`This is the result: "${result}"`);
+      computerChoiceHeading= document.createElement('h1')
+      computerChoiceHeading.innerHTML=`Computer Choice: ${computerValue}`
+      document.body.appendChild(computerChoiceHeading)
 
-function getComputerChoice() {
+      resultHeading = document.createElement('h1')
+      resultHeading.innerHTML=`Result: ${result}`
+      document.body.appendChild(resultHeading)
+    })
+  })
+ function getComputerChoice() {
     let number = Math.floor(Math.random() * 3) + 1;
 
     if (number === 1) {
@@ -16,7 +47,6 @@ function getComputerChoice() {
     }
 
 }
-
 function round(playerSelection, computerChoice) {
     if (playerSelection == "ROCK" && computerChoice == "PAPER") {
         return "You lost";
@@ -41,19 +71,13 @@ function round(playerSelection, computerChoice) {
     }
 }
 
-function game(){
-    for(let i = 0; i < 5; i++) {
-        playerValue = prompt("Type your choice: ").toUpperCase();
-        computerValue = getComputerChoice().toUpperCase();
-        result = round(playerValue,computerValue);
-        console.log(result);
-    }
-}
-
-game();
+})
 
 
 
-//console.log(`Computer value is: ${computerValue}`);
-//console.log(`Player value is: ${playerValue}`);
+
+
+
+
+
 
