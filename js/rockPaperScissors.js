d6303921ad98e54349288566a7672e73e944b3f3
Dileeka <dileekairanjandasanayake@gmail.com>
2022-12-31 18:04:09 +0530

    Added divs to index.html

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -1,111 +1,82 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-
-
-  let playerValue= ''
-  let computerValue = '';
-  let result = '';
-  const buttons = ["Rock","Paper","Scissor"]
-  let random = ''
-  let word = ''
-  const nButtons = 3
-  let resultHeading = ''
-  let computerChoiceHeading = ''
+  let computerSelection = ""
+  let playerSelection = ""
+  let result = ""
+  let computerScore = 0
+  let playerScore = 0
+  let randomNumber = 0
   let score = []
   let playerResult = 0
   let computerResult = 0
-  for(let i = 0; i<nButtons; i++ ){
-    random = Math.floor(Math.random()*(buttons.length-1))+1
-    word = buttons[random]
-    const newButton = document.createElement('button')
-    newButton.innerText=buttons[i]
-    document.body.appendChild(newButton)
-  }
-  let Mybuttons = document.querySelectorAll('button')
-  Mybuttons.forEach(i=>{
-    i.addEventListener('click',(e)=>{
-      playerValue= e.target.innerText
-      computerValue = getComputerChoice().toUpperCase();
-      result = round(playerValue.toUpperCase(),computerValue);
-      console.log(playerValue)
-      console.log(computerValue)
-      console.log(`This is the result: "${result}"`);
-      computerChoiceHeading= document.createElement('h1')
-      computerChoiceHeading.innerHTML=`Computer Choice: ${computerValue}`
-      document.body.appendChild(computerChoiceHeading)
+  let buttons = document.querySelectorAll('button').forEach(button => {
+    button.onclick = () => {
+      playerSelection = button.innerText.toLowerCase()
+      alert(playerSelection)
+      computerSelection = getComputerChoice().toLowerCase()
+      console.log(`computer selection :${computerSelection}`)
+      result = round(playerSelection,computerSelection)
+      score = getScores(result)
+      console.log(`round result is: ${result}`)
 
-      resultHeading = document.createElement('h1')
-      resultHeading.innerHTML=`Result: ${result}`
-      document.body.appendChild(resultHeading)
+      playerResult = playerResult + score[0]
+      computerResult = computerResult + score[1]
+      console.log(`playerResult: ${playerResult}`)
+      console.log(`compueter Result: ${computerResult}`)
+      let playerScore = document.querySelector('.playerScore')
+      let computerScore = document.querySelector('.computerScore')
+      playerScore.innerText = playerResult 
+      computerScore.innerText = computerResult 
 
-      score = getScore(result)
-      console.log(score[0])
-      console.log(score[1])
-      playerResult = playerResult + score[1]
-      computerResult= computerResult + score[0]
-      console.log(playerResult)
-      console.log(computerResult)
+      //deciding winner
       if(playerResult === 5){
-        alert(`Congratulations you've won, your score is ${playerResult}`)
+        alert(`congratulations you won! your result is ${playerResult}`)
         return
       }else if(computerResult === 5){
-        alert(`You Lost, your score is ${playerResult}`)
+        alert(`Opps You Lost!, May Be another time. your score is ${playerResult}`)
         return
       }
-    })
+    }
   })
- function getComputerChoice() {
-    let number = Math.floor(Math.random() * 3) + 1;
 
-    if (number === 1) {
-        return "rock";
-    }else if (number === 2) {
-        return "paper";
-    }else {
-        return "scissor";
+  //generate computer choice
+  function getComputerChoice(){
+   randomNumber = Math.floor(Math.random()*3) + 1
+    if(randomNumber === 1){
+      return "rock"
+    }else if(randomNumber === 2) {
+      return "paper"
+    }else if(randomNumber === 3) {
+      return "scissor"
     }
+  }
 
-}
-function round(playerSelection, computerChoice) {
-    if (playerSelection == "ROCK" && computerChoice == "PAPER") {
-        return "You lost";
-    } else if  (playerSelection == "ROCK" && computerChoice == "SCISSOR"){
-        return "You won";
-    } else if  (playerSelection == computerChoice){
-        return "Tied";
+  function round(playerChoice, computerChoice){
+    if(playerChoice === "rock" && computerChoice === "paper"){
+      return "lost"
+    }else if(playerChoice === "rock" && computerChoice === "scissor"){
+      return "won"
+    }else if(playerChoice === "paper" && computerChoice === "scissor"){
+      return "lost"
+    }else if(playerChoice === "paper" && computerChoice === "rock"){
+      return "won"
+    }else if(playerChoice === "scissor" && computerChoice === "rock"){
+      return "lost"
+    }else if(playerChoice === "scissor" && computerChoice === "paper"){
+      return "won"
+    }else if(playerChoice === computerChoice){
+      return "tied"
     }
-    if(playerSelection == "SCISSOR" && computerChoice == "ROCK") {
-        return "You lost";
-    } else if  (playerSelection == "SCISSOR" && computerChoice == "PAPER"){
-        return "You won";
-    } else if  (playerSelection == computerChoice){
-        return "Tied";
-    }
-    if(playerSelection == "PAPER" && computerChoice == "SCISSOR") {
-        return "You lost";
-    } else if  (playerSelection == "PAPER" && computerChoice == "ROCK"){
-        return "You won";
-    } else if  (playerSelection == computerChoice){
-        return "Tied";
-    }
-}
+  }
 
-  function getScore(aScore){
+  //get each score
+  function getScores(aScore){
     let computerTotal = 0
     let playerTotal = 0
-    if(aScore.toUpperCase() === "YOU LOST"){
+    if(aScore.toLowerCase() === "lost"){
       computerTotal++
-    }else if(aScore.toUpperCase() === "YOU WON"){
+    }else if(aScore.toLowerCase() === "won"){
       playerTotal++
     }
-    return [computerTotal, playerTotal]
+    return [playerTotal, computerTotal]
   }
-})
-
-
-
-
-
-
-
-
-
+}) 
